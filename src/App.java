87346323af36781ba23d3f493bf7374038f5535a
SAMIR-U <samir.borda@uptc.edu.co>
2026-05-27 18:54:34 -0500

    first try to conect to BD. lib and test script added.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Connection conn = DriverManager.getConnection(
+            "192.168.1.1:15022/sebq", 
+            "system",
+            "Z4P4T0Z0!"
+        );
+        PreparedStatement pSt = conn.prepareStatement(
+            "SELECT * FROM TAB"
+        );
+
+        ResultSet rs = pSt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(rs);
+        }
     }
 }
