a623b26668ff1cb34c914263c4f116fadcc158bd
SAMIR-U <samir.borda@uptc.edu.co>
2026-05-27 19:40:46 -0500

    test script complete!

--- src/App.java
+++ src/App.java
@@ -5,19 +5,33 @@ import java.sql.ResultSet;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Connection conn = DriverManager.getConnection(
-            "192.168.1.1:15022/sebq", 
-            "system",
-            "Z4P4T0Z0!"
-        );
-        PreparedStatement pSt = conn.prepareStatement(
-            "SELECT * FROM TAB"
-        );
+        try {
+            Connection conn = DriverManager.getConnection(
+                "jdbc:oracle:thin:@192.168.1.12:15022/sebq", 
+                "system",
+                "Z4P4T0Z0!"
+            );
+            
+            PreparedStatement pSt = conn.prepareStatement(
+                "SELECT sysdate AS fecha_actual, '¡Conexión exitosa!' AS mensaje FROM dual"
+            );
 
-        ResultSet rs = pSt.executeQuery();
+            ResultSet rs = pSt.executeQuery();
 
-        while (rs.next()) {
-            System.out.println(rs);
+            while (rs.next()) {
+                String fecha = rs.getString("fecha_actual");
+                String msj = rs.getString("mensaje");
+                
+                System.out.println("Fecha del servidor virtual: " + fecha);
+                System.out.println("Mensaje de Oracle: " + msj);
+            }
+
+            rs.close();
+            pSt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Hubo un error de conexión:");
+            e.printStackTrace();
         }
     }
 }
