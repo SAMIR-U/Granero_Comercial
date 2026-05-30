444ec9d72ab6cd88e84cf9b325122e4004e337a2
SAMIR-U <samir.borda@uptc.edu.co>
2026-05-30 17:52:10 -0500

    Login view logical part implemented

--- src/co/elgranero/view/Login.java
+++ src/co/elgranero/view/Login.java
@@ -2,37 +2,54 @@ package co.elgranero.view;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Login extends JFrame{
-    private boolean waiting;
-    private JPasswordField passwordField;
     private JTextField id;
+    private JPasswordField passwordField;
     private JButton submit;
+    private JLabel tries;
+    
+    private boolean waiting;
     private String password;
     private String identifier;
 
     public Login(){
+        //JFrame configs
+
         this.waiting = true;
         this.password = null;
-        this.submit = new JButton("Iniciar");
+        this.identifier = null;
+    
+        this.submit = new JButton("Entrar");
+        this.id = new JTextField();
+        this.tries = new JLabel();
+
         this.submit.addActionListener(e->{
             identifier = id.getText();
-            new String(passwordField.getPassword());
+            password = new String(passwordField.getPassword());
+            waiting = false;
         });
     }
 
-    public String getCredentials(){
-        return password;
+    public String[] getCredentials(){
+        return new String[]{identifier,password};
     }
 
     public boolean isWaiting() {
         return this.waiting;
     }
 
-    public void setValidLogin(boolean result) {
-        
+    public void setValidLogin(boolean isValid, int numTries) {
+        if (isValid) {
+            //show correct login
+            //close frame just the frame
+        }else{
+            //show error login
+            waiting = true;
+            tries.setText("Intentos restantes: "+numTries);
+        }
     }
-    
 }
