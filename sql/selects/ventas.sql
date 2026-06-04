ba8076e9d5d1d602181f1ac90b46210bd03d384c
SAMIR-U <samir.borda@uptc.edu.co>
2026-06-03 20:13:36 -0500

    Ventas select query corretion and element paths' name corretion

--- sql/selects/ventas.sql
+++ sql/selects/ventas.sql
@@ -1,6 +1,16 @@
-SELECT v.id_venta, v.id_forma_pago, v.per_id_cliente, v.fecha_venta, v.descuento_venta, 
+SELECT v.id_venta, 
+       v.id_forma_pago, 
+       v.per_id_cliente, 
+       p.nombre_cliente,
+       v.fecha_venta, 
+       v.descuento_venta, 
        COALESCE(SUM(vp.cantidad), 0) AS numero_de_productos 
 FROM VENTAS v
+JOIN PERSONAS p ON v.per_id_cliente = p.id_cliente -- 
 LEFT JOIN VENTAS_PRODUCTOS vp ON v.id_venta = vp.id_venta
-GROUP BY v.id_venta, v.id_forma_pago, v.per_id_cliente, v.fecha_venta, v.descuento_venta
-
+GROUP BY v.id_venta, 
+         v.id_forma_pago, 
+         v.per_id_cliente, 
+         p.nombre_cliente,
+         v.fecha_venta, 
+         v.descuento_venta
